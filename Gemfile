720d10c808d2c48e71c47f97bfd7575eeb714bc5
unknown <dbelcon@ssdnoc05-HP.ssdel.com>
2017-11-28 16:00:31 -0500

    Seeds added for Clients

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'uglifier', '>= 1.3.0'
 
 gem 'bootstrap', '~> 4.0.0.beta2.1'
 gem 'jquery-rails'
+gem 'database_cleaner'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+DatabaseCleaner.clean_with(:truncation)
+
+Client.delete_all
+Software.delete_all
+Server.delete_all
+
 clients = [
-    [ "Germany"],
-    [ "France", 65447374 ],
-    [ "Belgium", 10839905 ],
-    [ "Netherlands", 16680000 ]
+    [ "A Better Chance for Our Children"], [ "A1 Sanitate"], [ "Int House"], [ "Mid Atlantic"], [ "YWCA"], [ "Erdner"],
+    [ "Back 2 Basics"], [ "Baird Mandelas"], [ "BBB"], [ "Burns"], [ "Commlegal"], [ "Cooper P&H"], [ "DE Foundation"],
+    [ "DHA"], [ "Elsmere"], [ "Fieldstone"], [ "IDA"], [ "Jacobs"], [ "MHA"], [ "Revision"], [ "Schagrin"], [ "SSD"],
+    [ "Tarabicos"], [ "Tevebaugh"], [ "Therapy Source"], [ "Walker"], [ "DCADV"], [ "Mendoza"], [ "Schagrin"], [ "Sparsons"],
+    [ "HRS"], [ "Vigilant"], [ "NKS"], [ "DVLS"], [ "Lex Electric"], [ "Creative Commerce"], [ "Intrust"], [ "Advita"],
+    [ "Wilson Safe"], [ "Shawkeller"], [ "Fisher"], [ "Disabatino"], [ "W S Cumby"], [ "DHIN"], [ "US Roofing"], [ "DE Surgery"],
+    [ "Emory Hill"], [ "Habitat"], [ "Autism"], [ "Breslin"], [ "Erdner"], [ "Minquas Fire Company"], [ "Seiberlich"],
+    [ "Div Financial"], [ "Vigilant"], [ "C&C Drywall"], [ "Community Education Building"], [ "Wilmington Montesseri School"],
+    [ "Environmental Alliance"], [ "Cirillobro"], [ "Mishimoto"], [ "Town of Townsend"]
 ]
 
-clients.each do |name, population|
-  Country.create( name: name, population: population )
+
+
+
+clients.each do |name|
+  Client.create(name: name)
 end
+
